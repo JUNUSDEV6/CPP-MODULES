00e1af2c8a29d73b2c706c810d14a7b92d210b0f
Younes Hanafi <younes-hanafi97@hotmail.com>
2024-10-09 15:32:20 +0200

    ex02

--- CP00/ex02/Account.cpp
+++ CP00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 13:54:36 by yohanafi          #+#    #+#             */
-/*   Updated: 2024/10/09 15:05:59 by yohanafi         ###   ########.fr       */
+/*   Updated: 2024/10/09 15:32:00 by yohanafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,33 @@ Account::~Account(void)
 {
 	_displayTimestamp();
 	std::cout << " index:" << _accountIndex << ";amount:" << _amount << ";closed" << std::endl;
+}
+
+void	Account::_displayTimestamp(void)
+{
+	std::time_t now = std::time(0);
+	std::tm *ltm = std::localtime(&now);
+	std::cout	<< "[" << (ltm->tm_year + 1900)
+				<< std::setw(2) << std::setfill('0') << (ltm->tm_mon + 1)
+				<< std::setw(2) << std::setfill('0') << ltm->tm_mday
+				<< std::setw(2) << std::setfill('0') << ltm->tm_hour
+				<< std::setw(2) << std::setfill('0') << ltm->tm_min
+				<< std::setw(2) << std::setfill('0') << ltm->tm_sec << "]";
+}
+
+void	Account::displayAccountsInfos(void)
+{
+	_displayTimestamp();
+	std::cout	<< "accounts:" << _nbAccounts
+				<< ";total:" << _totalAmount
+				<< ";deposits" << _nbDeposits
+				<< ";withdrawals:" << _nbWithdrawals << std::endl;
+}
+
+void	Account::makeDeposit(int deposit)
+{
+	_displayTimestamp();
+	std::cout	<< "index" << _nbAccounts
+				<< ";amount:" << _amount
+				<< ";deposits:" << _nbDeposits 
 }
