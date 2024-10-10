8d3fdba4237febda19333f02844c184d90de2c92
Younes Hanafi <younes-hanafi97@hotmail.com>
2024-10-10 17:06:33 +0200

    cp01

--- CP01/ex00/Zombie.cpp
+++ CP01/ex00/Zombie.cpp
@@ -1,18 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   newZombie.cpp                                      :+:      :+:    :+:   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 16:22:31 by yohanafi          #+#    #+#             */
-/*   Updated: 2024/10/10 17:01:25 by yohanafi         ###   ########.fr       */
+/*   Updated: 2024/10/10 16:59:26 by yohanafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie *newZombie(std::string name)
+Zombie::Zombie(std::string name)
 {
-	return new Zombie(name);
+	this->name = name;
+	std::cout	<< "contructor called" << std::endl;
+}
+
+Zombie::~Zombie(void)
+{
+	std::cout << "destructor called" << std::endl;
+}
+
+void	Zombie::announce(void)
+{
+	std::cout << this->name << ": " << "BraiiiiiiinnnzzzZ..." << std::endl;
 }
