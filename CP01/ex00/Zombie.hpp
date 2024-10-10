fefc269c67f325e7f7923d77f0a5f93a9f710282
Younes Hanafi <younes-hanafi97@hotmail.com>
2024-10-10 15:55:50 +0200

    cp01

---
+++ CP01/ex00/Zombie.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 15:40:21 by yohanafi          #+#    #+#             */
+/*   Updated: 2024/10/10 15:55:30 by yohanafi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+# include <string>
+# include <iostream>
+
+class	Zombie
+{
+	public:
+
+		Zombie(void);
+		~Zombie(void);
+		
+		void	announce(void);
+
+	private:
+	
+		std::string	name;
+};
+
+Zombie	*newZombie(std::string name);
+void	randomChump(std::string name);
+
+#endif
