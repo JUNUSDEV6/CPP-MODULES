9172f09c0daa4e49ceadead9faf544710527d6af
Younes Hanafi <younes-hanafi97@hotmail.com>
2024-10-10 12:11:09 +0200

    fini

--- CP00/ex02/Account.cpp
+++ CP00/ex02/Account.cpp
@@ -6,15 +6,18 @@
 /*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 13:54:36 by yohanafi          #+#    #+#             */
-/*   Updated: 2024/10/10 11:57:33 by yohanafi         ###   ########.fr       */
+/*   Updated: 2024/10/10 12:10:02 by yohanafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Account.hpp"
 #include <iostream>
+#include <iomanip>
+#include <ctime> 
 
-int	Account::_accountIndex = 0;
-int	Account::_amount = 0;
-int	Account::_nbDeposits = 0;
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
 int	Account::_totalNbWithdrawals = 0;
 
 Account::Account(int initial_deposit): _accountIndex(_nbAccounts), _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0)
@@ -48,8 +51,8 @@ void	Account::displayAccountsInfos(void)
 	_displayTimestamp();
 	std::cout	<< "accounts:" << _nbAccounts
 				<< ";total:" << _totalAmount
-				<< ";deposits" << _nbDeposits
-				<< ";withdrawals:" << _nbWithdrawals << std::endl;
+				<< ";deposits" << _totalNbDeposits
+				<< ";withdrawals:" << _totalNbWithdrawals << std::endl;
 }
 
 void	Account::makeDeposit(int deposit)
@@ -88,7 +91,37 @@ bool	Account::makeWithdrawal(int withdrawal)
 	return true;
 }
 
-int	Account::checkAmount()
+int	Account::checkAmount(void) const
 {
 	return _amount;
+}
+
+void	Account::displayStatus(void) const
+{
+	_displayTimestamp();
+	std::cout	<< " index:" << _accountIndex
+				<< ";amount:" << _amount
+				<< ";deposits:" << _nbDeposits
+				<< ";withdrawals:" << _nbWithdrawals
+				<< std::endl;
+}
+
+int	Account::getNbAccounts(void)
+{
+	return _nbAccounts;
+}
+
+int Account::getTotalAmount(void)
+{
+	return _totalAmount;
+}
+
+int Account::getNbDeposits(void)
+{
+	return _totalNbDeposits;
+}
+
+int	Account::getNbWithdrawals(void)
+{
+	return _totalNbWithdrawals;
 }
