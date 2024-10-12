e1dcca54dd30710dd8c65212b552584bb2137895
Younes Hanafi <younes-hanafi97@hotmail.com>
2024-10-12 15:09:59 +0200

    ex04

---
+++ CP01/ex04/Sed.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Sed.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/12 14:57:12 by yohanafi          #+#    #+#             */
+/*   Updated: 2024/10/12 15:06:11 by yohanafi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SED_HPP
+# define SED_HPP
+# include <string>
+
+class	Sed
+{
+	public:
+
+		Sed(const std::string& filename, std::string& s1, std::string& s2);
+		~Sed(void);
+
+		
+	private:
+
+		std::string	filename;
+		std::string	s1;
+		std::string	s2;
+
+};
+
+#endif
