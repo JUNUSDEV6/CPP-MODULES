afe5a8ef465282752641b3c5e886ec3883b978af
Younes Hanafi <younes-hanafi97@hotmail.com>
2024-10-12 16:52:14 +0200

    ex04

--- CP01/ex04/Sed.cpp
+++ CP01/ex04/Sed.cpp
@@ -6,13 +6,30 @@
 /*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 15:06:19 by yohanafi          #+#    #+#             */
-/*   Updated: 2024/10/12 15:09:47 by yohanafi         ###   ########.fr       */
+/*   Updated: 2024/10/12 16:52:03 by yohanafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Sed.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
 
-Sed::Sed(const std::string& filename, std::string& s1, std::string& s2) : filename(filename), s1(s1), s2(s2)
+Sed::Sed(const std::string& filename, std::string& s1, std::string& s2)
 {
+	std::ifstream	inputfile(filename);
+	if (!inputfile)
+		std::cerr << "Error with the open file process" << std::endl;
+		
+	std::string fileContent;
+	std::string	line;
+	while (std::getline(inputfile, line))
+		fileContent += line + "\n";
+	inputfile.close();
 	
+	std::string	newContent;
+	size_t		pos;
+	size_t		s1length;
+	s1length = s1.length();
+	git
 }
