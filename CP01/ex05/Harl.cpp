3ee7743b842978619b80e34c9b0e782989e23f90
JUNUSDEV6 <younes-hanafi97@hotmail.com>
2024-11-18 15:36:22 +0100

    ex05

--- CP01/ex05/Harl.cpp
+++ CP01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: yohanafi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 13:05:20 by yohanafi          #+#    #+#             */
-/*   Updated: 2024/11/14 17:17:52 by yohanafi         ###   ########.fr       */
+/*   Updated: 2024/11/18 15:29:55 by yohanafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,26 @@ Harl::Harl(void)
 Harl::~Harl(void)
 {
 	std::cout << "destructor Harl called" << std::endl;
+}
+
+void	Harl::debug(void)
+{
+	std::cout << "DEBUG: I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+}
+
+void	Harl::info(void)
+{
+	std::cout << "INFO: I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void	Harl::warning(void)
+{
+	std::cout << "WARNING: I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void	Harl::error(void)
+{
+	std::cout << "ERROR: This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
 void	Harl::complain(std::string level)
---
+++ CP01/ex05/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yohanafi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/18 15:30:37 by yohanafi          #+#    #+#             */
+/*   Updated: 2024/11/18 15:33:13 by yohanafi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int	main(void)
+{
+	Harl	harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	return 0;
+}
