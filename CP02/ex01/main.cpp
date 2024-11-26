03cde88ce4a4fce972ce6ad82fb35172506fa122
younes hanafi <younes-hanafi97@hotmail.com>
2024-11-26 17:13:14 +0100

    e02

---
+++ CP02/ex01/main.cpp
@@ -0,0 +1,15 @@
+
+int	main(void)
+{
+	Fixed		a;
+	Fixed const	b(10);
+	Fixed const	c(42.42f);
+	Fixed const	d(b);
+
+	a = Fixed(1234.4321f);
+
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << a << std::endl;
+	std::cout << "c is " << a << std::endl;
+	std::cout << "d is " << a << std::endl;
+}
