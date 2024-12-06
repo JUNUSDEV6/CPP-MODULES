f2d0d956453737baa75618a517a84934f3481da9
younes hanafi <younes-hanafi97@hotmail.com>
2024-12-06 16:16:40 +0100

    ex02

---
+++ CP02/ex02/Fixed.cpp
@@ -0,0 +1,45 @@
+#include "Fixed.hpp"
+
+
+Fixed::Fixed(void)
+{
+	std::cout << "Constructor Fixed Called" << std::endl; 
+}
+
+Fixed::Fixed(const int nb)
+{
+	value = nb << bits;
+}
+
+Fixed::Fixed(const float nb)
+{
+	value = static_cast<int>(roundf(nb * (1 << bits)));
+}
+
+Fixed& Fixed::operator=(const Fixed& other)
+{
+	if (this != &other)
+		this->value = other.value;
+	return *this;
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+	std::cout << "copy constructor called" << std::endl;
+	*this = other;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "destructor called" << std::endl;
+}
+
+float Fixed::toFloat(void) const
+{
+	return static_cast<float>(value) / (1 << bits);
+}
+
+int Fixed::toInt(void) const
+{
+	return value >> bits;
+}
