db797ba032c347e4df93338e77790913dbfdb246
younes hanafi <younes-hanafi97@hotmail.com>
2024-12-13 18:31:42 +0100

    ex00

--- CP03/ex00/ClapTrap.cpp
+++ CP03/ex00/ClapTrap.cpp
@@ -15,14 +15,37 @@ void	ClapTrap::attack(const std::string& target)
 	if (_HitPoints > 0 && _EnergyPoints > 0)
 	{
 		_EnergyPoints--;
-		std::cout << "ClapTrap :" << _name << "attack " << target << "and causing " << _attackDamage << 
-			" amount of damage " << std::endl;
+		std::cout << "ClapTrap :" << _name << "attack " << target << "and causing " << _attackDamage << " amount of damage " << std::endl;
+	}
+	else
+		std::cout << "ClapTrap " << _name << "can't attack because insufficiency of amount " << _EnergyPoints << "energy" << "or" << _HitPoints << "healt point" <<  std::endl;
+}
+
+void	ClapTrap::takeDamage(unsigned int amount)
+{
+	if ((_HitPoints - amount) >= 0)
+	{
+		_HitPoints -= amount;
+		std::cout << "ClapTrap " << _name << "loose of amount :" << amount << "HitPoints now is amount of Hitpoints" << _HitPoints << std::endl;
 	}
 	else
 	{
-		std::cout << "ClapTrap " << _name << "can't attack because insufficiency of amount " <<
-			_EnergyPoints << "energy" << std::endl;
+		_HitPoints = 0;
+		std::cout << "ClapTrap " << _name << "loose of amount :" << amount << "HitPoints now is amount of Hitpoints" << _HitPoints << std::endl;
+
 	}
+}
+
+void	ClapTrap::BeRepaired(unsigned int amount)
+{
+	if (_HitPoints > 0 && _EnergyPoints > 0)
+	{
+		_EnergyPoints--;
+		_HitPoints += amount;
+		std::cout << "ClapTrap " << _name << "take care of amount " << amount << " now his HitPoints " << _HitPoints << std::endl;
+	}
+	else
+		std::cout << "ClapTrap " << _name << "can't attack because insufficiency of amount " << _EnergyPoints << "energy" << "or" << _HitPoints << "healt point" <<  std::endl;
 }
 
 
