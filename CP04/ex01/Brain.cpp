f0eb90e60c935cc311e59aa3a4dd0be95e7608f8
younes hanafi <younes-hanafi97@hotmail.com>
2025-01-16 13:04:01 +0100

    ex01

---
+++ CP04/ex01/Brain.cpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Brain.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/16 12:40:06 by yohanafi          #+#    #+#             */
+/*   Updated: 2025/01/16 13:03:34 by yohanafi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Brain.hpp"
+
+Brain::Brain(void)
+{
+    std::cout << "Brain Contructeur Called" << std::endl;
+}
+
+Brain::Brain(const Brain& other)
+{
+    std::cout << "contructor copied called" << std::endl;
+    *this = other;
+}
+
+Brain& Brain::operator=(const Brain& other)
+{
+    if (this != &other)
+    {
+        for(int i = 0, i < 100, ++i)
+            this->ideas[i] = other.ideas[i];
+    }
+    return *this;
+}
+
+Brain::~Brain(void)
+{
+    std::cout << "Destructor Brain called" << std::endl;
+}
+
+void        Brain::setIdeas(int index, const std::string& idea)
+{
+    if (index >= 0 && index <= 100)
+        this->ideas[index] = idea;
+}
+
+std::string brain::getIdeas(int index) const
+{
+    if (index >= 0 && index <= 100)
+        return this->ideas[index];
+    return "";
+}
--- CP04/ex01/Brain.hpp
+++ CP04/ex01/Brain.hpp
@@ -6,7 +6,7 @@
 /*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 17:32:45 by yohanafi          #+#    #+#             */
-/*   Updated: 2025/01/15 17:38:42 by yohanafi         ###   ########.fr       */
+/*   Updated: 2025/01/16 12:56:34 by yohanafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,12 @@ class   Brain
         Brain(const Brain& other);
         Brain& operator=(const Brain& other);
         virtual ~Brain(void);
+
+        void        setIdeas(int index, const std::string& idea)
+        std::string getIdeas(int index) const;
+
+    private:
+        std::string     ideas[100];
 
         
 }
