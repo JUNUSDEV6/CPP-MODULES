c42c2ebd9c59abaa19cfd320a7f65df175391340
younes hanafi <younes-hanafi97@hotmail.com>
2025-01-21 14:33:20 +0100

    ex04

--- CP04/ex03/MateriaSource.cpp
+++ CP04/ex03/MateriaSource.cpp
@@ -6,7 +6,7 @@
 /*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 12:33:38 by yohanafi          #+#    #+#             */
-/*   Updated: 2025/01/21 13:29:59 by yohanafi         ###   ########.fr       */
+/*   Updated: 2025/01/21 14:28:51 by yohanafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,6 +68,8 @@ AMateria*   AMateria::createMateria(std::string const &type)
 {
     for (int i = 0; i < 4; i++)
     {
-        (_template[i] &&)
+        (_template[i] && _template[i]->getType() == type)
+            return _template[i]->clone();
     }
+    return NULL;
 }
