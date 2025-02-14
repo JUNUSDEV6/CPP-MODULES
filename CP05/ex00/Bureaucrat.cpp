f27fa4fec3d3eee71f81108666b9dd7d18309750
JUNUSDEV6 <younes-hanafi97@hotmail.com>
2025-02-14 17:31:02 +0100

    back

--- CP05/ex00/Bureaucrat.cpp
+++ CP05/ex00/Bureaucrat.cpp
@@ -6,30 +6,53 @@
 /*   By: youneshanafi <youneshanafi@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 17:59:56 by yohanafi          #+#    #+#             */
-/*   Updated: 2025/02/14 15:03:16 by youneshanaf      ###   ########.fr       */
+/*   Updated: 2025/02/14 17:30:53 by youneshanaf      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat(void)
+Bureaucrat::Bureaucrat(const std::string  &name, int grade) : _name(name), _grade(grade)
 {
     std::cout << "Bureaucrat Contructor Called" << std::endl;
 }
-
-
 
 Bureaucrat::~Bureaucrat(void)
 {
     std::cout << "Bureaucrat Destructor Called" << std::endl;
 }
 
+const char	*Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return "Grade is too high, the grade must be between 1 & 150 !";
+}
+
+const char	*Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return	"Grade is too low, the grade must be between 1 & 150 !";
+}
 std::string Bureaucrat::getName(void) const
 {
     return _name;
 }
 
-std::string Bureaucrat::getGrade(void) const
+int		Bureaucrat::getGrade(void) const
 {
     return  _grade;
+}
+
+void	Bureaucrat::incrGrade(void)
+{
+	if (_grade + 1 > 150)
+		throw	GradeTooHighException();
+	else
+		_grade++;
+}
+
+void	Bureaucrat::decrGrade(void)
+{
+	if (_grade - 1 < 1)
+		throw	GradeTooLowException();
+	else
+		_grade--;
 }
--- CP05/ex00/Bureaucrat.hpp
+++ CP05/ex00/Bureaucrat.hpp
@@ -6,32 +6,42 @@
 /*   By: youneshanafi <youneshanafi@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 17:41:18 by yohanafi          #+#    #+#             */
-/*   Updated: 2025/02/14 15:12:14 by youneshanaf      ###   ########.fr       */
+/*   Updated: 2025/02/14 17:28:09 by youneshanaf      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUREAUCAT_HPP
 # define BUREAUCAT_HPP
 # include <iostream>
+
 class   Bureaucrat
 {
     public:
-        Bureaucrat(void);
-        Bureaucrat(const Bureaucrat& other);
-        Bureaucrat& operator=(const Bureaucrat& other);
+        Bureaucrat(const std::string &name, int grade);
         ~Bureaucrat(void);
 
-        Bureaucrat GradeTooHighException();
-        Bureaucrat GradeTooLowException();
+		class	GradeTooHighException : std::exception
+		{
+			public:
+        		virtual const char *what() const throw();
+    	};
+
+		class	GradeTooLowException : std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+
         std::string getName(void) const;
-        std::string getGrade(void) const;
-        void incre(Bureaucrat);
-        void        decre(bure)
+        int         getGrade(void) const;
+        void        incrGrade(void);
+        void        decrGrade(void);
+
     
     private:
         std::string _name;
-        std::string _grade[150];
+        int         _grade;
 
-}
+};
 
 #endif
