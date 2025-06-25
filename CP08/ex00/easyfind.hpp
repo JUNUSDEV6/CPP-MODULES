fb510e7953ac1bb3465385d908a9d43c9b45cafa
younes hanafi <younes-hanafi97@hotmail.com>
2025-06-25 18:43:06 +0200

    ex00

---
+++ CP08/ex00/easyfind.hpp
@@ -0,0 +1,13 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <algorithm>
+#include <stdexcept>
+
+template <typename T>
+
+typename T::const_iterator easyfind(const T& container, int value);
+
+#include <easyfind.tpp>
+
+#endif
