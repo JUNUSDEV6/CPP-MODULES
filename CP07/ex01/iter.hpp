ec1ed615d5f75f27a1eb918157051e94d052e8a9
younes hanafi <younes-hanafi97@hotmail.com>
2025-07-11 12:06:37 +0200

    1

--- CP07/ex01/iter.hpp
+++ CP07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 16:40:19 by yohanafi          #+#    #+#             */
-/*   Updated: 2025/07/04 16:50:46 by yohanafi         ###   ########.fr       */
+/*   Updated: 2025/07/11 12:00:47 by yohanafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,13 @@ void    iter(T *array, size_t len, Func func)
     for (size_t i = 0; i < len; i++)
         func(array[i]);
 }
+
+template<typename T, typename Func>
+void    iter(const T *array, size_t len, Func func)
+{
+    for (size_t i = 0; i < len; i++)
+        func(array[i]);
+}
+
 
 #endif 
--- CP07/ex01/main.cpp
+++ CP07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 14:44:48 by youneshanaf       #+#    #+#             */
-/*   Updated: 2025/07/09 15:30:59 by yohanafi         ###   ########.fr       */
+/*   Updated: 2025/07/11 12:03:07 by yohanafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,11 @@
 
 
 void printInt(int n) 
+{
+    std::cout << n << " ";
+}
+
+void printIntC(const int n) 
 {
     std::cout << n << " ";
 }
@@ -29,26 +34,30 @@ void increment(int &n)
     n += 1;
 }
 
+
+
 int main() 
 {
+    const int intArrayC[] = {1, 2, 3, 4, 5};
     int intArray[] = {1, 2, 3, 4, 5};
-    char charArray[] = {'a', 'b', 'c', 'd', 'e'};
+    const char charArray[] = {'a', 'b', 'c', 'd', 'e'}; // const ici
+
     size_t intSize = sizeof(intArray) / sizeof(int);
     size_t charSize = sizeof(charArray) / sizeof(char);
 
     std::cout << "Int array: ";
-    iter(intArray, intSize, printInt);
+    iter(intArrayC, intSize, printIntC); // non const, modifiable
     std::cout << std::endl;
 
-    std::cout << "Char array: ";
-    iter(charArray, charSize, printChar);
+    std::cout << "Char array (const): ";
+    iter(charArray, charSize, printChar); // const, non modifiable
     std::cout << std::endl;
 
     std::cout << "Incrementing int array..." << std::endl;
-    iter(intArray, intSize, increment);
+    iter(intArray, intSize, increment); // OK car modifiable
 
     std::cout << "Modified int array: ";
-    iter(intArray, intSize, printInt);
+    iter(intArray, intSize, printIntC);
     std::cout << std::endl;
 
     return 0;
