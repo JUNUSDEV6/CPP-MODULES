6b14dc640830cd62f4419227cc1266234b564260
younes hanafi <younes-hanafi97@hotmail.com>
2025-07-15 20:04:39 +0200

    ex02

---
+++ CP08/ex02/MutantStack.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/15 19:08:31 by yohanafi          #+#    #+#             */
+/*   Updated: 2025/07/15 20:04:28 by yohanafi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+template <typename T>
+class   MutantStack : public : std::stack<T>
+{
+    public:
+        typedef typename    std::stack<T>::container_type::iterator                 iterator;
+        typedef typename    std::stack<T>::container_type::const_iterator           const_iterator;
+        typedef typename    std::stack<T>::container_type::reverse_iterator         reverse_iterator;
+        typedef typename    std::stack<T>::container_type::const_reverse_iterator   const_reverse_iterator;
+
+        MutantStack(void);
+        MutantStack(const MutantStack& other);
+        MutantStack &operator=(const MutantStack& other);
+        ~MutantStack(void);
+
+        iterator                begin();
+        iterator                end();
+
+        const_iterator          begin() const;
+        const_iterator          end() const;
+        
+        reverse_iterator        rbegin();
+        reverse_iterator        rend();
+        
+        const_reverse_iterator  rbegin() const;
+        const_reverse_iterator  rend() const;
+};
+
+#endif
