0548e2df2f07fed99aa001f5cb01acf43e71b1e6
younes hanafi <younes-hanafi97@hotmail.com>
2025-07-16 16:43:09 +0200

    1

---
+++ exam/vect2/vect2.cpp
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vect2.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 15:02:21 by yohanafi          #+#    #+#             */
+/*   Updated: 2025/07/16 16:42:59 by yohanafi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Vect2.hpp"
+
+Vect2::Vect2(void) :_x(0), _y(0) {}
+
+Vect2::Vect2(int x, int y) : _x(x) , _y(y) {}
+
+Vect2::Vect2(const Vect2& other) : _x(other._x), _y(other._y) {}
+
+Vect2   Vect2::&operator=(const Vect2& other) {
+    if (*this != other)
+    {
+        _x = other._x;
+        _y = other._y;
+    }
+    return *this;
+}
+
+Vect2::~Vect2(void) {}
+
+int Vect2::&operator[](int i) {
+    return (i == 0 ? _x : _y);
+}
+
+int Vect2::&operator[](int i) const {
+    return (i == 0 ? _x : _y);
+}
+
+Vect2   Vect2::operator++(int) {
+    int tmp = *this;
+    _x++;
+    _y++;
+    return tmp;
+}
+
+Vect2   Vect2::&operator++() {
+    _x++;
+    _y++;
+    return *this;
+}
+
+Vect2   Vect2::operator--(int) {
+    int tmp = *this;
+    _x--;
+    _y--;
+    return *this;
+}
+
+Vect2   Vect2::&operator--() {
+    _x--;
+    _y--;
+    return *this;
+}
+
+Vect2&  Vect2::operator+=(const Vect2& other) {
+    _x += other._x;
+    _y += other._y;
+    return *this;
+}
+
+Vect2&  Vect2::operator-=(const Vect2& other){}
---
+++ exam/vect2/vect2.hpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vect2.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 14:19:57 by yohanafi          #+#    #+#             */
+/*   Updated: 2025/07/16 16:12:14 by yohanafi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECT2_HPP
+#define VECT2_HPP
+
+class   Vect2
+{
+    public:
+        Vect2(void);
+        Vect2(int x, int y);
+        Vect2(const Vect2& other);
+        Vect2   &operator=(const Vect2& other);
+        ~Vect2(void);
+
+        int &operator[](int i);
+        int &operator[](int i) const;
+
+        Vect2   operator++(int);
+        Vect2   &operator++();
+        Vect2   operator--(int);
+        Vect2   &operator--();
+
+        Vect2&  operator+=(const &Vect2 other);
+        Vect2&  operator-=(const &Vect2 other);
+
+        Vect2   operator+(const &Vect2 other) const;
+        Vect2   operator-(const &Vect2 other) const;
+
+        Vect2   operator*(int i) const;
+        Vect2   operator*=(int i);
+
+        Vect2&  operator-();
+
+        bool    operator==(const Vect2& other);
+        bool    operator!=(const Vect2& other);
+        
+        friend  Vect2   operator*(int s, const Vect2 v);
+        friend  std::ostream&   operator<<(std::ostream& os, const Vect2& v);
+        
+    private:
+        int _x;
+        int _y;
+};
+
+#endif
